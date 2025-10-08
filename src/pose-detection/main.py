480d46baa11dfc54e922112e4fe7950eed1ad1a4
Matvey Solovev <m.solovev5@g.nsu.ru>
2025-10-08 09:56:33 +0700

    Реализовал базовове определение позы человека по фото

---
+++ src/pose-detection/main.py
@@ -0,0 +1,45 @@
+import mediapipe as mp
+from mediapipe.tasks import python
+from mediapipe.tasks.python import vision
+from mediapipe import solutions
+from mediapipe.framework.formats import landmark_pb2
+import numpy as np
+import cv2
+
+def main():
+    base_options = python.BaseOptions(model_asset_path="models/pose_landmarker_full.task")
+    options = vision.PoseLandmarkerOptions(
+        base_options=base_options,
+        output_segmentation_masks=True)
+    detector = vision.PoseLandmarker.create_from_options(options)
+
+    cv_input = cv2.imread("assets/image2.png")
+    image = mp.Image(image_format=mp.ImageFormat.SRGB, data=cv_input)
+
+    detection_result = detector.detect(image)
+    annotated_image = draw_landmarks_on_image(image.numpy_view(), detection_result)
+    processed_image = cv2.cvtColor(annotated_image, cv2.COLOR_BGR2RGB)
+    cv2.imwrite("ouput.jpg", annotated_image)
+
+def draw_landmarks_on_image(rgb_image, detection_result):
+  pose_landmarks_list = detection_result.pose_landmarks
+  annotated_image = np.copy(rgb_image)
+
+  # Loop through the detected poses to visualize.
+  for idx in range(len(pose_landmarks_list)):
+    pose_landmarks = pose_landmarks_list[idx]
+
+    # Draw the pose landmarks.
+    pose_landmarks_proto = landmark_pb2.NormalizedLandmarkList()
+    pose_landmarks_proto.landmark.extend([
+      landmark_pb2.NormalizedLandmark(x=landmark.x, y=landmark.y, z=landmark.z) for landmark in pose_landmarks
+    ])
+    solutions.drawing_utils.draw_landmarks(
+      annotated_image,
+      pose_landmarks_proto,
+      solutions.pose.POSE_CONNECTIONS,
+      solutions.drawing_styles.get_default_pose_landmarks_style())
+  return annotated_image
+
+if __name__ == "__main__":
+    main()
