100245a734543ca601d7087136be0cedd0bf530e
Artemius951 <a.kutsenko1@g.nsu.ru>
2025-12-20 18:07:55 +0700

    fixed warning

--- src/PPEMG/EMG_Calibration.py
+++ src/PPEMG/EMG_Calibration.py
@@ -246,7 +246,7 @@ class EMGCalibrator:
         """
         
         try:
-            self.mqtt_client = mqtt.Client()
+            self.mqtt_client = mqtt.Client(mqtt.CallbackAPIVersion.VERSION2)
             
             self.mqtt_client.on_connect = self.on_mqtt_connect
             self.mqtt_client.on_message = self.on_mqtt_message
