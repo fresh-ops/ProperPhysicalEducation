00cdfe510c366d79de93dfb85c910c52cfc7d5aa
Ruslana Babich <r.babich@g.nsu.ru>
2025-12-23 19:38:54 +0700

    extract emg filter and delete test data

--- src/Server/server/server.py
+++ src/Server/server/server.py
@@ -2,68 +2,52 @@ import os
 import paho.mqtt.client as mqtt
 import struct 
 import time
-import random
 import json
+from emg_filter import EMGFilter
 
 # Чтение переменных окружения для конфигурации
 MQTT_BROKER = os.getenv('MQTT_BROKER', 'mqtt-broker')
 MQTT_PORT = int(os.getenv('MQTT_PORT', 1883))
 
-# Константы для фильтрации
-last = 0.0
-Data = 0.0
-lastD = 0.0
-fk = 0.3
+EMG_DATA_TOPIC = "emg/raw"
+EMG_FILTERED_DATA_TOPIC = "emg/filtered"
+CAMERA_POINTS_TOPIC = "camera/points"
 
-# Функция для отправки тестовых точек
-def send_test_points():
-    # Генерируем 33 точки [x, y, z]
-    test_points = [[random.random(), random.random(), random.random()] for _ in range(33)]
-    # Отправляем как JSON
-    client.publish("camera/points", json.dumps(test_points))
-
-
-# Экспоненциальное скользящее среднее (фильтр)
-def filter_ema(raw_value):
-    global last, Data, lastD, fk
-    lastD = Data
-    Data = abs(last - raw_value) * fk + Data * (1.0 - fk)
-    last = raw_value
-    return Data
-
-# Подключение к MQTT брокеру по топикам
-def on_connect(client, userdata, flags, rc):
-    """Вызывается при установлении соединения с брокером."""
-    if rc == 0:
-        print("Connected with result code " + str(rc))
-        client.subscribe("emg/raw")
-        # client.subscribe("camera/points")
+def on_connect(client, userdata, flags, reason_code):
+    if reason_code == 0:
+        print(f"Connected with result code {reason_code}")
+        client.subscribe(EMG_DATA_TOPIC)
+        client.subscribe(CAMERA_POINTS_TOPIC)
     else:
-        print("Failed to connect, return code " + str(rc))
+        print(f"Failed to connect, return code {reason_code}")
 
-# Обработка входящих сообщений
-def on_message(client, userdata, msg):
-    #emg
-    if msg.topic == "emg/raw":
-        try:
-            print(f"[RECV] topic={msg.topic} payload={msg.payload}")
+def on_message(client, userdata, message):
+    match message.topic:
+        case EMG_DATA_TOPIC:
+            try:
+                if len(message.payload) != 2:
+                    return
 
-            if len(msg.payload) != 2:
-                return
-
-            value_tuple = struct.unpack('<H', msg.payload)
-            raw_value = float(value_tuple[0])
-            filtered_value = filter_ema(raw_value)
+                value_tuple = struct.unpack('<H', message.payload)
+                raw_value = float(value_tuple[0])
+                filtered_value = filter.filter(raw_value)
             
-            client.publish("emg/filtered", str(filtered_value))
-            print(f"[EMG] raw={raw_value} filtered={filtered_value}")
+                client.publish(EMG_FILTERED_DATA_TOPIC, str(filtered_value))
 
-        except Exception as e:
-            print("EMG Error:", e)
+            except Exception as e:
+                print(f"EMG_DATA_TOPIC error: {e}")
+        case CAMERA_POINTS_TOPIC:
+            try:
+                points = json.loads(message.payload.decode('utf-8'))
+                print(points)
+            except Exception as e:
+                print(f"CAMERA_POINTS_TOPIC error: {e}")
+
 
 
 # Инициализация MQTT клиента
 client = mqtt.Client()
+filter = EMGFilter()
 client.on_connect = on_connect
 client.on_message = on_message
 
@@ -86,11 +70,4 @@ for attempt in range(max_retries):
             exit(1)
 
 print("Connected successfully! Starting loop...")
-client.loop_start() 
-try:
-    while True:
-        send_test_points() # Генерируем точки для AngleAnalyzer
-        time.sleep(0.5)  
-except KeyboardInterrupt:
-    client.loop_stop()
-
+client.loop_start()
