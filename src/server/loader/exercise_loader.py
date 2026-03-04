5da1dd3898da92968afbdba863e85b94d9c278c8
Ruslana Babich <r.babich@g.nsu.ru>
2026-03-04 11:06:29 +0700

    #49 add exercise model, add exercise loader, add exercise json template

---
+++ src/server/loader/exercise_loader.py
@@ -0,0 +1,77 @@
+import json
+from pathlib import Path
+from loader.pose_loader import PoseLoader
+from model.exercise import Exercise
+
+
+class ExerciseLoader:
+    """
+    Загрузчик упражнений из JSON файлов
+    """
+
+
+    def __init__(self, pose_loader: PoseLoader, directory_path: str = '.'):
+        """
+        Инициализирует загрузчик упражнений.
+
+        Args:
+            pose_loader (PoseLoader): загрузчик поз для получения поз по ID
+            directory_path (str): путь к директории с упражнениями
+
+        Raises:
+            ValueError: если путь не является валидной директорией
+        """
+        self.pose_loader = pose_loader
+        self.directory = Path(directory_path)
+        if not self.directory.is_dir():
+            raise ValueError(f"'{directory_path}' is not a valid directory.")
+
+
+    def load_exercise(self, exercise_id: int) -> Exercise:
+        """
+        Загружает упражнение в формате JSON из директории и десериализует его.
+
+        Args:
+            exercise_id (int): ID упражнения для загрузки.
+
+        Returns:
+            Exercise: загруженное упражнение
+        """
+
+        for json_file in self.directory.glob("*.json"):
+            try:
+                with open(json_file, 'r', encoding='utf-8') as f:
+                    serialized_exercise = json.load(f)
+                    if serialized_exercise["id"] == exercise_id:
+                        return self.__deserialize_exercise(serialized_exercise)
+                    
+            except TypeError as e:
+                print(f"Field mismatch in {json_file}: {e}")
+            except json.JSONDecodeError as e:
+                print(f"Invalid JSON in {json_file}: {e}")
+            except Exception as e:  
+                print(f"Error reading {json_file}: {e}")
+        raise ValueError(f"Exercise with id {exercise_id} not found in directory '{self.directory}'")
+    
+
+    def __deserialize_exercise(self, serialized_exercise: dict) -> Exercise:
+        """
+        Десериализует упражнение из словаря, загруженного из JSON.
+
+        Args:
+            serialized_exercise (dict): словарь с данными упражнения
+        
+        Returns:
+            Exercise: десериализованное упражнение
+        """
+        loaded_poses = [
+            self.pose_loader.load_pose(id) 
+            for id in serialized_exercise["poses"]
+        ]
+
+        return Exercise(
+            id = serialized_exercise["id"],
+            name = serialized_exercise["name"],
+            poses = loaded_poses
+        )
+    
---
+++ src/server/model/exercise.py
@@ -0,0 +1,16 @@
+from typing import NamedTuple
+from model.pose import Pose
+
+
+class Exercise(NamedTuple):
+    """
+        Класс упражнения. Содержит в себе информацию об упражнении, а также эталонные позы для этого упражнения.
+
+        Fields:
+            id (int): идентификатор упражнения
+            name (str): имя упражнения
+            poses (list[Pose]): список эталонных поз для этого упражнения
+    """
+    id: int    
+    name: str
+    poses: list[Pose]
