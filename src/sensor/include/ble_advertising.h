f039525ec5d2083046120e717103d0b2ffd11458
Artemius951 <a.kutsenko1@g.nsu.ru>
2026-03-10 20:45:36 +0700

    sending data from sensors with ble

---
+++ src/sensor/include/ble_advertising.h
@@ -0,0 +1,5 @@
+#pragma once
+
+#include <cstdint>
+
+void start_ble_advertising(const char *device_name, uint16_t service_uuid_16);
---
+++ src/sensor/include/ble_config.h
@@ -0,0 +1,7 @@
+#pragma once
+
+#include <cstdint>
+
+constexpr char DEVICE_NAME[] = "PPE Sensor";
+constexpr uint16_t SERVICE_UUID_16 = 0x503E;
+constexpr uint16_t CHAR_UUID_BASE = 0xA000;
