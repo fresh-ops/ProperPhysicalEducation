b454b1badee08b7f5947cd43c4b82acd02189d88
Ruslana Babich <r.babich@g.nsu.ru>
2026-03-17 01:32:16 +0700

    #49 extract exercise state machine

--- src/server/analyzer/exercise/exercise_analyzer.py
+++ src/server/analyzer/exercise/exercise_analyzer.py
@@ -1,28 +1,21 @@
+from analyzer.exercise.exercise_state_machine import ExerciseStateMachine
 from analyzer.pose.pose_matcher.pose_matcher import PoseMatcher
 from model.pose import Pose
 from model.exercise import Exercise
 
 
 class ExerciseAnalyzer:
-    def __init__(self, exercise: Exercise, pose_matcher: PoseMatcher):
+    def __init__(
+        self,
+        exercise: Exercise,
+        pose_matcher: PoseMatcher,
+        exercise_state_machine: ExerciseStateMachine,
+    ):
         self.poses = exercise.poses
-        self.current_pose_index = 0
         self.pose_detector = pose_matcher
+        self.exercise_state_machine = exercise_state_machine
 
     def analyze(self, current_pose: Pose) -> int:
-        current_reference_pose: Pose = self.poses[self.current_pose_index]
-        next_pose_index: int = (self.current_pose_index + 1) % len(self.poses)
-
-        match_result = self.pose_detector.match(current_pose)
-        closest_reference_pose = match_result.pose
-        if closest_reference_pose.id == current_reference_pose.id:
-            if current_pose == current_reference_pose:
-                self.current_pose_index = next_pose_index
-                print("Correct pose!")
-                return 0
-            else:
-                print("Almost correct pose. Try to adjust your position.")
-                return 1
-        else:
-            print("Incorrect pose. Try again.")
-            return 1
+        matched_pose = self.pose_detector.match(current_pose)
+        exercise_state = self.exercise_state_machine.update(matched_pose.pose)
+        return exercise_state.current_pose_index
---
+++ src/server/analyzer/exercise/exercise_state.py
@@ -0,0 +1,4 @@
+class ExerciseState:
+    def __init__(self) -> None:
+        self.current_pose_index = 0
+        self.hold_frame_count = 0
