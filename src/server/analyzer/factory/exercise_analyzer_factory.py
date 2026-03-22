a1f9b16f0398d3e9e21c27c7c0999323d074efe5
Ruslana Babich <r.babich@g.nsu.ru>
2026-03-22 17:44:52 +0700

    fix analyzer

--- src/server/analyzer/factory/exercise_analyzer_factory.py
+++ src/server/analyzer/factory/exercise_analyzer_factory.py
@@ -1,4 +1,5 @@
 from analyzer.exercise.exercise_analyzer import ExerciseAnalyzer
+from analyzer.exercise.exercise_state import ExerciseState
 from analyzer.exercise.exercise_state_machine import ExerciseStateMachine
 from analyzer.feedback.feedback_generator import FeedbackGenerator
 from analyzer.feedback.rule_evaluator import RuleEvaluator
@@ -23,11 +24,14 @@ class ExerciseAnalyzerFactory:
     def create(self, exercise_id: int) -> ExerciseAnalyzer:
         exercise = self.exercise_loader.load_exercise(exercise_id)
         pose_matcher = PoseMatcher(exercise.poses, self.match_strategy)
+        exercise_state = ExerciseState()
         return ExerciseAnalyzer(
             exercise=exercise,
             pose_matcher=pose_matcher,
             exercise_state_machine=ExerciseStateMachine(
-                exercise.poses, settings.frame_tolerance
+                exercise_state=exercise_state,
+                poses=exercise.poses,
+                frame_tolerance=settings.frame_tolerance,
             ),
             feedback_generator=FeedbackGenerator(RuleEvaluator(exercise.rules)),
         )
