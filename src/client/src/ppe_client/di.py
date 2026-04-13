15335ff2b570ad8d3d6616a95825cb0202eb4287
Matvey Solovev <m.solovev5@g.nsu.ru>
2026-04-13 21:01:59 +0700

    add: put all services to the di container
    Signed-off-by: Matvey Solovev <m.solovev5@g.nsu.ru>

--- src/client/src/ppe_client/di.py
+++ src/client/src/ppe_client/di.py
@@ -1,7 +1,32 @@
 from wireup import SyncContainer, create_sync_container, injectable
 
 from ppe_client import presentation
+from ppe_client.adapters.cameras import (
+    RefCountedCameraSessionStorage,
+    SessionTerminator,
+)
+from ppe_client.adapters.cameras.open_cv import (
+    OpenCVCameraEnumerator,
+    OpenCVCameraSessionFactory,
+)
 from ppe_client.adapters.network import ExerciseSession
+from ppe_client.adapters.poses import MediaPipePoseDetectorFactory
+from ppe_client.adapters.poses.restoration import PoseRestorer
+from ppe_client.application.cameras import CameraSessionService
+from ppe_client.application.cameras.ports import (
+    CameraEnumerator,
+    CameraSessionFactory,
+    CameraSessionStorage,
+)
+from ppe_client.application.poses import PoseService
+from ppe_client.application.poses.ports import PoseDetectorFactory, PoseReciever
+
+
+@injectable
+def make_pose_service(
+    detector_factory: PoseDetectorFactory, restorer: PoseRestorer
+) -> PoseService:
+    return PoseService(detector_factory, restorer)
 
 
 @injectable
@@ -9,15 +34,24 @@ def make_exercise_session() -> ExerciseSession:
     return ExerciseSession()
 
 
+injectables = [
+    make_exercise_session,
+    injectable(ExerciseSession, as_type=PoseReciever),
+    injectable(OpenCVCameraEnumerator, as_type=CameraEnumerator),
+    injectable(SessionTerminator),
+    injectable(RefCountedCameraSessionStorage, as_type=CameraSessionStorage),
+    injectable(OpenCVCameraSessionFactory, as_type=CameraSessionFactory),
+    injectable(CameraSessionService),
+    injectable(MediaPipePoseDetectorFactory, as_type=PoseDetectorFactory),
+    injectable(PoseRestorer),
+    make_pose_service,
+    # ViewModels
+    presentation,
+]
+
+
 def create_container() -> SyncContainer:
     """
     Create a new DI container.
     """
-    return create_sync_container(
-        injectables=[
-            # Services
-            make_exercise_session,
-            # ViewModels
-            presentation,
-        ]
-    )
+    return create_sync_container(injectables=injectables)
