80a766dfd5f8b24e6f79a0a65c1e74b0c4019d29
Ruslana Babich <r.babich@g.nsu.ru>
2026-04-20 22:10:38 +0700

    update config

--- src/server/config.py
+++ src/server/config.py
@@ -2,9 +2,11 @@ from pydantic_settings import BaseSettings
 
 
 class Settings(BaseSettings):
+    redis_host: str = "localhost"
+    redis_port: int = 6379
     session_timeout_seconds: int = 60
-    exercise_data_path: str = "data/exercise"
-    pose_data_path: str = "data/pose"
+    exercise_data_path: str = "infrastructure/data/json/exercise"
+    pose_data_path: str = "infrastructure/data/json/pose"
     frame_tolerance: int = 3
 
     class Config:
