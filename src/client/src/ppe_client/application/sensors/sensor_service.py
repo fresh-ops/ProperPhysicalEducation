66da34d6fb06c20c30f43b0faf8e1e4409fef9e2
Matvey Solovev <m.solovev5@g.nsu.ru>
2026-05-01 17:13:25 +0700

    fix: remove connection logic from sensor service
    Signed-off-by: Matvey Solovev <m.solovev5@g.nsu.ru>

--- src/client/src/ppe_client/application/sensors/sensor_service.py
+++ src/client/src/ppe_client/application/sensors/sensor_service.py
@@ -22,22 +22,5 @@ class SensorService:
     async def discover(self, timeout_s: float = 2.0) -> list[SensorDescriptor]:
         return await self._registry.enumerate(timeout_s)
 
-    # TODO: remove connection logic
-    async def connect(self, descriptor: SensorDescriptor) -> bool:
-        try:
-            sensor = await self._registry.get(descriptor)
-            await sensor.connect()
-            return sensor.has_connections()
-        except Exception:
-            return False
-
-    async def disconnect(self, descriptor: SensorDescriptor) -> None:
-        sensor = await self.get_sensor(descriptor)
-        await sensor.disconnect()
-
-    async def is_connected(self, descriptor: SensorDescriptor) -> bool:
-        sensor = await self.get_sensor(descriptor)
-        return sensor.has_connections()
-
     async def get_sensor(self, descriptor: SensorDescriptor) -> Sensor:
         return await self._registry.get(descriptor)
