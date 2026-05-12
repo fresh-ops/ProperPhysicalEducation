a65a59b6f93df832e9339fe13655fb3a17a37541
Artemius951 <a.kutsenko1@g.nsu.ru>
2026-05-12 20:21:03 +0700

    old filter is better than new one

---
+++ src/client/src/ppe_client/adapters/sensors/ema_signal_filter.py
@@ -0,0 +1,23 @@
+class EMASignalFilter:
+    """Exponential Moving Average (EMA) filter for EMG signal filtering."""
+
+    def __init__(self, filter_coefficient: float = 0.3):
+        self.filter_coefficient = filter_coefficient
+        self.previous_raw_value = 0.0
+        self.previous_filtered_value = 0.0
+        self.filtered_value = 0.0
+
+    def filter(self, raw_value: float) -> float:
+        self.previous_filtered_value = self.filtered_value
+        self.filtered_value = abs(
+            self.previous_raw_value - raw_value
+        ) * self.filter_coefficient + self.filtered_value * (
+            1.0 - self.filter_coefficient
+        )
+        self.previous_raw_value = raw_value
+        return self.filtered_value
+
+    def reset(self) -> None:
+        self.previous_raw_value = 0.0
+        self.previous_filtered_value = 0.0
+        self.filtered_value = 0.0
